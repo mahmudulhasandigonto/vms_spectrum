cffdb99b678f5feb67a353b53c88c86172aad682
mahmudulhasandigonto <mahmudulhasandigonto@gmail.com>
2023-02-23 16:05:15 +0600

    FuelLogController error is solved

--- src/main/java/com/spectrum/spectrum_vms/controller/FuelLogController.java
+++ src/main/java/com/spectrum/spectrum_vms/controller/FuelLogController.java
@@ -9,6 +9,8 @@ import com.spectrum.spectrum_vms.service.FuelLogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,13 +25,13 @@ public class FuelLogController implements BaseController<FuelLog, Long> {
     final private FuelLogService fuelLogService;
 
     @Override
-    public ResponseEntity<FuelLog> save(FuelLog fuelLog) {
+    public ResponseEntity<FuelLog> save(@RequestBody FuelLog fuelLog) {
         fuelLogService.save(fuelLog);
         return  ResponseEntity.ok(fuelLog);
     }
 
     @Override
-    public ResponseEntity<String> update(FuelLog fuelLog) throws Exception {
+    public ResponseEntity<String> update(@RequestBody FuelLog fuelLog) throws Exception {
         try {
             fuelLogService.update(fuelLog);
             return ResponseEntity.ok("FuelLog information has been updated successfully");
@@ -39,7 +41,7 @@ public class FuelLogController implements BaseController<FuelLog, Long> {
     }
 
     @Override
-    public ResponseEntity<?> deleteByIds(Long... ids) throws DeleteRequestException {
+    public ResponseEntity<?> deleteByIds(@PathVariable("ids") Long... ids) throws DeleteRequestException {
         try {
             fuelLogService.deleteByIds(ids);
             return ResponseEntity.ok("ID " + Arrays.toString(ids) + " has been deleted successfully");
@@ -49,7 +51,7 @@ public class FuelLogController implements BaseController<FuelLog, Long> {
     }
 
     @Override
-    public ResponseEntity<List<FuelLog>> getDataByIds(Long... ids) {
+    public ResponseEntity<List<FuelLog>> getDataByIds(@PathVariable("ids") Long... ids) {
         List<FuelLog> fuelLongList = fuelLogService.getDataByIds(ids);
         return ResponseEntity.ok(fuelLongList);
     }
